c65c5025516fab1ca55ffde920346dcf191d3fa1
filmaj <maj.fil@gmail.com>
2018-02-01 09:25:05 -0600

    special case andela

--- companies.js
+++ companies.js
@@ -1,5 +1,5 @@
 module.exports = {
-    catch_all: /microsoft|google|amazon|amzn|aws|red hat|redhat|ibm|pivotal|intel|facebook|alibaba|uber|wix|github|tencent|baidu|apple|mozilla|oracle|shopify|mongodb|vmware|netflix|salesforce|linkedin|palantir|yahoo|mapbox|unity|automattic|travis|spotify|zalando|esri|sap|epam|telerik|stripe|kitware|suse|odoo|yandex|adobe|airbnb|guardian|docker|nuxeo|nvidia|elastic|yelp|wso2|inria|puppet|datadog|jetbrains|canonical|rackspace|thoughtworks/gi,
+    catch_all: /microsoft|google|amazon|amzn|aws|red hat|redhat|ibm|pivotal|intel|facebook|alibaba|uber|wix|github|tencent|baidu|apple|mozilla|oracle|shopify|mongodb|vmware|netflix|salesforce|linkedin|palantir|yahoo|mapbox|unity|automattic|travis|spotify|zalando|esri|sap|epam|telerik|stripe|kitware|suse|odoo|yandex|adobe|airbnb|guardian|docker|nuxeo|nvidia|elastic|yelp|wso2|inria|puppet|datadog|jetbrains|canonical|rackspace|thoughtworks|andela/gi,
     map: {
         microsoft: 'Microsoft',
         google: 'Google',
@@ -59,6 +59,7 @@ module.exports = {
         jetbrains: 'JetBrains',
         canonical: 'Canonical',
         rackspace: 'Rackspace',
-        thoughtworks: 'ThoughtWorks'
+        thoughtworks: 'ThoughtWorks',
+        andela: 'Andela'
     }
 };
