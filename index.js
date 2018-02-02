977a836695adb8f83422f4b29b0e5d75049bf12b
filmaj <maj.fil@gmail.com>
2018-02-02 09:01:35 -0600

    dont bomb the whole program if saving to bigquery fails. worst case, catch error, dont update the row marker, and keep going.

--- index.js
+++ index.js
@@ -92,8 +92,9 @@ const target_table = dataset.table(USERS_TO_COMPANIES); // this table is where w
             process.stdout.write('Processed ' + counter + ' records in ' + end_time.from(start_time, true) + '                     \r');
             if (counter % 1000 === 0 && !persistence.is_saving()) {
                 // Every X records, lets flush the new rows to bigquery, unless were already saving/flushing.
-                await persistence.save_rows_to_bigquery(target_table, row_marker, new_rows);
-                new_rows = [];
+                let did_persist = await persistence.save_rows_to_bigquery(target_table, row_marker, new_rows);
+                // if saving to bigquery worked, flush out new_rows array. otherwise, hope we get it next time.
+                if (did_persist) new_rows = [];
             }
         }
         console.log('Processed', counter, 'records in', end_time.from(start_time, true), '.');
--- persistence.js
+++ persistence.js
@@ -8,12 +8,18 @@ module.exports.save_rows_to_bigquery = async (target_table, row_marker, new_rows
     try {
         insert_op = await target_table.insert(new_rows);
     } catch (e) {
-        console.error('Error inserting rows! Oh no!', e);
-        throw e;
+        console.warn('Error inserting rows! Oh no! Will try to ignore...', e);
+        insert_op = null;
     }
-    console.log('', new_rows.length, 'user-company records saved to BigQuery!', insert_op);
-    // save our row marker to disk
-    await row_module.write(row_marker);
+    if (insert_op) {
+        console.log('', new_rows.length, 'user-company records saved to BigQuery!', insert_op);
+        // save our row marker to disk
+        await row_module.write(row_marker);
+    }
     is_saving = false;
     if (bomb) process.exit(0);
+    else {
+        if (insert_op) return true;
+        else return false;
+    }
 };
