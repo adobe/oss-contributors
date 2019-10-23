8e75174c0152964bd866dee83975708317c8e9e2
filmaj <maj.fil@gmail.com>
2019-10-23 11:22:55 -0400

    tweak cli to eliminate db.json

--- bin/oss.js
+++ bin/oss.js
@@ -17,38 +17,21 @@ const update_db = require('../src/update-db.js');
 const rank = require('../src/rank.js');
 
 yargs
-    .command('db-to-json <output>', 'create a (huge) json blob of user-to-company associations based on database records', {
-        output: {
-            alias: 'o',
-            demandOption: 'You must specify an output file to write to.',
-            desc: 'Output file to write DB to (in JSON)'
-        }
-    }, db_to_json)
-    .command('json-to-bigquery <input> <output>', 'send a JSON blob of user-to-company associations to a bigquery table', {
-        input: {
-            alias: 'i',
-            demandOption: 'You must specify an input file to read from, the output of the `db-to-json` command.',
-            desc: 'JSON file to send to BigQuery'
-        },
+    .command('db-to-bigquery <output>', 'send a JSON blob of user-to-company associations to a bigquery table', {
         output: {
             alias: 'o',
             default: 'users_companies',
             desc: 'BigQuery table to send data to'
         }
     }, json_to_bigquery)
-    .command('update-db <source> [db-json]', 'update user-to-company database based on source-table, optionally providing a db.json db cache (to speed things up)', {
+    .command('update-db <source>', 'update user-to-company database based on source-table', {
         source: {
             alias: 's',
             demandOption: 'You must provide a BigQuery table name as a GitHub.com activity source!',
             desc: 'BigQuery table name housing GitHub.com activity data'
-        },
-        'db-json': {
-            alias: 'j',
-            demandOption: 'You must provide a db.json cache file.',
-            desc: 'Local file storing GitHub username to company associations (with ETags), used to speed up processing'
         }
     }, update_db)
-    .command('rank <source> [limit] [db-json]', 'show top [limit] companies based on number of active GitHubbers, parsed from the <source> BigQuery table', {
+    .command('rank <source> [limit]', 'show top [limit] companies based on number of active GitHubbers, parsed from the <source> BigQuery table', {
         source: {
             alias: 's',
             demandOption: 'You must provide a BigQuery table name as a GitHub.com activity source!',
@@ -59,11 +42,6 @@ yargs
             desc: 'How many top companies to show?',
             default: null,
             defaultDescription: 'Shows up to and including Adobe'
-        },
-        'db-json': {
-            alias: 'j',
-            demandOption: 'You must provide a db.json cache file.',
-            desc: 'Local file storing GitHub username to company associations (with ETags), used to speed up processing'
         }
     }, rank)
     .env('OSS')
